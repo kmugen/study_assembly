2f9a9feffce6245684ebe341dee6e47c7c50f685
kmugen <102893260+kmugen@users.noreply.github.com>
2023-11-21 19:27:37 +0900

    Update main.asm

--- 005_call/main.asm
+++ 005_call/main.asm
@@ -5,15 +5,15 @@ section .text
 
 _main:
 		mov		rcx, 1
-		call	_sub_routine		; call: return to where sub routine was called
-		mov		rcx, 3				; unlike jmp, the line be executed
-		jmp		_next				; jmp: doesn't return
-		mov		rcx, 4				; unlike call, the line not be executed
+		call		_sub_routine		; call: return to where sub routine was called
+		mov		rcx, 3			; unlike jmp, the line be executed
+		jmp		_next			; jmp: doesn't return
+		mov		rcx, 4			; unlike call, the line not be executed
 _next:
 		mov		rax, 0x2000001
 		mov		rdi, 0
 		syscall
 
 _sub_routine:
-		mov		rcx, 2				; execute the line then back to line 9
+		mov		rcx, 2			; execute the line then back to line 9
 		ret
