d237233cebead4f3958aec26ffd91df5ebec6236
kmugen <102893260+kmugen@users.noreply.github.com>
2023-11-21 19:27:04 +0900

    Style: Replace space with tab

--- 001_exit/main.asm
+++ 001_exit/main.asm
@@ -4,6 +4,6 @@ section .text
 		global _main
 
 _main:
-		mov		rax, 0x2000001		; system call number (0, exit) + 0x2000000 (Unix BSD assembly)
-		mov		rdx, 0 				; exit status
+		mov		rax, 0x2000001	; system call number (0, exit) + 0x2000000 (Unix BSD assembly)
+		mov		rdx, 0		; exit status
 		syscall
