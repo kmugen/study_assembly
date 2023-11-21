521eb40465a86ed7a67ea50533da06fc931b1d58
kmugen <102893260+kmugen@users.noreply.github.com>
2023-11-21 19:26:24 +0900

    Style: Replace space with tab

--- 003_read/main.asm
+++ 003_read/main.asm
@@ -12,7 +12,7 @@ section .text
 
 _main:
 		; print msg_1 on terminal
-		mov 	rax, 0x2000004
+		mov		rax, 0x2000004
 		mov		rdi, 1
 		mov		rsi, msg_1
 		mov		rdx, 15
@@ -20,26 +20,26 @@ _main:
 
 		; get name from terminal
 		mov		rax, 0x2000003		; system call number (3, sys_read)
-		mov		rdi, 0 				; file descriptor (0, stdin)
+		mov		rdi, 0			; file descriptor (0, stdin)
 		mov		rsi, buf_name		; buffer address
-		mov		rdx, 16				; buffer size
+		mov		rdx, 16			; buffer size
 		syscall
 
 		; print msg_2 on terminal
-		mov 	rax, 0x2000004
+		mov		rax, 0x2000004
 		mov		rdi, 1
 		mov		rsi, msg_2
 		mov		rdx, 13
 		syscall
 
 		; print buf_name on terminal
-		mov 	rax, 0x2000004
+		mov		rax, 0x2000004
 		mov		rdi, 1
 		mov		rsi, buf_name
 		mov		rdx, 15
 		syscall
 
 		; exit system
-		mov 	rax, 0x2000001
-		mov 	rdi, 0
+		mov		rax, 0x2000001
+		mov		rdi, 0
 		syscall
